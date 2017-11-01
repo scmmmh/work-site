50a1278ae66302e0a8b3e59d9477f01a9f75eddf
Mark <Mark.Hall@edgehill.ac.uk>
2017-11-01 12:08:15 +0000

    Format the build script output a bit cleaner

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 echo "Content-Type: text/html"
 echo ""
+echo "<!DOCTYPE html><html><head><title>Rebuild</title></head><body><ul><li>"
 hg pull --update
+echo "</li><li>"
 npm install
+echo "</li><li>"
 ./node_modules/.bin/gulp --gulpfile ./theme-gulpfile.js
+echo "</li><li>"
 source ./venv/bin/activate
 pip install -r requirements.txt
+echo "</li><li>"
 ./venv/bin/pelican content -s publishconf.py
+echo "</li></ul></body></html>"
