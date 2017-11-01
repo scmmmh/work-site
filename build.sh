d25d9e937ab8f9b9755c09e29e278de8d1885577
Mark <Mark.Hall@edgehill.ac.uk>
2017-11-01 11:41:54 +0000

    Fixed paths in the build script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 hg pull --update
 npm install
-./node_modules/.bin/gulp --gulpfile theme-gulpfile.js
+./node_modules/.bin/gulp --gulpfile ./theme-gulpfile.js
 source ./venv/bin/activate
 pip install -r requirements.txt
-pelican content -s publishconf.py
+./venv/bin/pelican content -s publishconf.py
