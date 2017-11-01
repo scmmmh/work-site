8fd51be3bdb32c435ef79a86e292c7a39b29e285
Mark <Mark.Hall@edgehill.ac.uk>
2017-11-01 11:44:01 +0000

    Added missing NPM dependency

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ npm install
 ./node_modules/.bin/gulp --gulpfile ./theme-gulpfile.js
 source ./venv/bin/activate
 pip install -r requirements.txt
-./venv/bin/pelican content -s publishconf.py
+./venv/bin/pelican content -s publishconf.py
