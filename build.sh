a6e037eeb995ff087ad97f3cbb92c6c84e76ba5c
Mark Hall <mark.hall@work.room3b.eu>
2020-06-19 21:48:54 +0100

    Fixed the build script

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ export PIPENV_VENV_IN_PROJECT=1
 git pull
 yarn install
 ./node_modules/.bin/gulp
-pipenv install
-pipenv run pelican -o output -d -s publishconf.py content
+poetry install
+poetry run pelican -o output -d -s publishconf.py content
